26bcdd9ad12ccd4d9a647e92ab7b2a33478f6ff5
Dmitry Yurov <aliersis@gmail.com>
2016-02-22 20:28:44 +0300

    Tests for basic command acceptance

---
+++ lib/parts/command_interface.rb
@@ -0,0 +1,2 @@
+class CommandInterface
+end
---
+++ spec/parts/command_interface_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require_relative '../../spec/spec_helper'
+require_relative '../../lib/parts/command_interface'
+
+describe CommandInterface do
+  before :all do
+    @interface = CommandInterface.new
+  end
+
+  it 'should accept text command' do
+    command = 'status'
+    expect { @interface.send_command(command) }.not_to raise_error
+  end
+
+  it 'should raise an error on unknown command' do
+    command = 'do something'
+    expect { @interface.send_command(command) }.to raise_error { |error|
+      expect(error).to be_a(CommandInputError)
+    }
+  end
+
+  it 'should raise an error on empty command' do
+    command = '   '
+    expect { @interface.send_command(command) }.to raise_error { |error|
+      expect(error).to be_a(CommandInputError)
+    }
+  end
+
+  it 'should raise an error on nil command' do
+    command = nil
+    expect { @interface.send_command(command) }.to raise_error { |error|
+      expect(error).to be_a(CommandInputError)
+    }
+  end
+end
