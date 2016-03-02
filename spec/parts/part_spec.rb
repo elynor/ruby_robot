5a738d824ba80fdb10a76286a61543626c2d4244
Dmitry Yurov <aliersis@gmail.com>
2016-03-02 22:57:40 +0300

    Added basic part tests

---
+++ spec/parts/part_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require_relative '../../spec/spec_helper'
+require_relative '../../lib/parts/part'
+
+describe Part do
+  before :all do
+    @part = Part.new
+  end
+
+  it 'should parse class name' do
+    expect(@part.name).to eq('part')
+  end
+
+  it 'should load config' do
+    expect(@part.config).not_to be_empty
+  end
+
+  it 'should load commands in array' do
+    expect(@part.commands).not_to be_empty
+    expect(@part.commands).to be_instance_of(Array)
+  end
+
+  it 'should load proper default commands' do
+    expect(@part.commands).to include('status')
+  end
+end
